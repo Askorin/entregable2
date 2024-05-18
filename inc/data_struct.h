74846a736e2e9d3f4f3ceac8a5d58d403eeeb289
Antonio Skorin <antonio.skorin@gmail.com>
2024-05-17 23:22:19 -0400

    Update: data_struct modificado

--- inc/data_struct.h
+++ inc/data_struct.h
@@ -3,7 +3,10 @@
 #include <string>
 
 struct data_struct {
-public:
+
+
+    bool valid;
+
     std::string university;
     /* 64 bits */
     unsigned long long user_id;
@@ -21,6 +24,8 @@ public:
     data_struct(std::string university, unsigned long long user_id, std::string username,
             unsigned int n_tweets, unsigned int friend_count, unsigned int follower_count,
             std::string created_at);
+
+    void setValid(bool valid);
 };
 
 std::ostream& operator << (std::ostream &os, const data_struct &elem);
